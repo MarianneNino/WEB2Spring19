3a63a3fe6bef88e8e0b1902173d4130da69b14ea
Marianne <ma9008@gmail.com>
2019-05-27 09:05:14 -0700

    Added js, made some changes in the main page style

---
+++ Final/main.js
@@ -0,0 +1,13 @@
+<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+
+    <script>
+    
+        $(document).ready(function() {
+            $("#show_hide").click(function () {
+                $( ".men_ex" ).toggle({
+            duration: 3000,
+            });
+            })  
+        })
+
+    </script>
