a9cf296c1293e5e64d460e2f0d0622968657044b
Marianne <ma9008@gmail.com>
2019-05-27 21:43:07 -0700

    donation form
    Building the donation form

--- Final/main.js
+++ Final/main.js
@@ -1,6 +1,3 @@
-<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-
-    <script>
     
     $(document).ready(function() {
         $("#show_hide").click(function () {
@@ -10,4 +7,58 @@
         })  
     })
 
-    </script>
+
+var firstName = "";
+		var lastName = "";
+		var email = "";
+		var dType = "";
+		var receipt = "";
+		var anon = "";
+		var list = "";
+		var amount = "";
+		
+		$('.set-amount').autoGrow(0);
+		
+		/*
+			if(isiPad || jQuery.browser.mobile){
+				$('#team').hide()
+				$('.team-mobile').show();	
+			}else{
+				$('#team').show()
+				$('.team-mobile').hide();
+			}
+		*/
+		
+		//Set & Highlight Donation Amount
+		$(".button").click(function(){
+			$(".button").removeClass("selected");
+			$(this).addClass("selected");
+			
+			$(this).find("input").focus();
+		});
+		
+		//Grow the donation box if they type more than 4 numbers
+		$(".set-amount").keyup(function(){
+			
+			if (this.value != this.value.replace(/[^0-9\.]/g, '')) {
+		       this.value = this.value.replace(/[^0-9\.]/g, '');
+		    }
+
+		});
+		
+		
+		$("input").on("change", function(){
+			// $(".donation-box").css("height", "500px");
+			
+			firstName = $("#firstName").val();
+			lastName = $("#lastName").val();
+			email = $("#email").val();
+			
+			if ( $("#one-time").prop( "checked" ) ){
+				dType = "One-Time";
+			}
+			if ( $("#monthly").prop( "checked" ) ){
+				dType = "Monthly";
+			}
+
+		});
